34a722dafc2ad29a32a666a2a495277fd5f58dad
Kelson Anthony <k.thony@gmail.com>
2018-06-07 22:56:22 -0300

    this is a jest test for unix

---
+++ sum.js
@@ -0,0 +1,16 @@
+'use strict'
+
+export default () => {
+    return 3
+}
+
+// export default () => {
+//     if (1 == 2) {
+//         return
+//     }
+// }
+// module.exports = () => {
+//     if (1 == 2) {
+//         return
+//     }
+// }
